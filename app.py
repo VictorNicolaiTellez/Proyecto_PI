768b28c418171dde7ff2414df18793c030a9c9cc
Javier Jiménez Vaquero <jjimeneznr@alumnos.unex.es>
2025-02-19 17:08:20 +0100

    Estructura de la API: templates añadidos (studio.html, login.html, signup.html) y corrección de errores

--- app.py
+++ app.py
@@ -13,13 +13,26 @@ def index():
 def songs():
     return render_template('songs.html')
 
+@app.route('/store/')
+def store():
+    return render_template('store.html')
+
 @app.route('/library/')
 def library():
     return render_template('library.html')
 
-@app.route('/store/')
-def store():
-    return render_template('store.html')
+@app.route('/studio/')
+def studio():
+    return render_template('studio.html')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup/')
+def signup():
+    return render_template('signup.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

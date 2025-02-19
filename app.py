5da50b6d601c81ed4682560e32e7920a1b113392
Javier Jimenez Vaquero <jjimeneznr@alumnos.unex.es>
2025-02-19 15:48:33 +0100

    Archivo app.py
    Archivo para ejecutar la API

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask import Flask, Blueprint
+from flask import redirect, url_for
+from flask import request, session,jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/songs/')
+def songs():
+    return render_template('songs.html')
+
+@app.route('/library/')
+def library():
+    return render_template('library.html')
+
+@app.route('/store/')
+def store():
+    return render_template('store.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

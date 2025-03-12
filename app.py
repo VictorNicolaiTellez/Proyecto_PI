005a5d9c41d6a5750b204e520888b20126b4f496
Danny5249 <95285856+Danny5249@users.noreply.github.com>
2025-03-12 17:33:27 +0100

    Studio y Upload song

--- app.py
+++ app.py
@@ -9,38 +9,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/explorer/', methods=['GET'])
-def music():
-    songs = [
-        {"id":"0","album":"Tribute","artist":"John Newman","duration":"4:00","genre":"0","name":"Love Me Again","rank_time":"2"},
-        {"id":"1","album":"V","artist":"Maroon 5","duration":"3:10","genre":"0","name":"Maps","rank_time":"1"},
-        {"id":"2","album":"A Head Full of Dreams","artist":"Coldplay","duration":"4:24","genre":"0","name":"Adventure of a Lifetime","rank_time":"1"},
-        {"id":"3","album":"Boulevard of Broken Dreams","artist":"Green Day","duration":"4:21","genre":"0","name":"Boulevard of Broken Dreams","rank_time":"2"},
-        {"id":"4","album":"V","artist":"Maroon 5","duration":"3:51","genre":"0","name":"Animals","rank_time":"1"},
-        {"id":"5","album":"Overexposed Track By Track","artist":"Maroon 5","duration":"3:40","genre":"0","name":"One More Night","rank_time":"0"},
-        {"id":"6","album":"(What's The Story) Morning Glory?","artist":"Oasis","duration":"4:19","genre":"0","name":"Wonderwall","rank_time":"0"},
-        {"id":"7","album":"Favourite Worst Nightmare","artist":"Arctic Monkeys","duration":"4:14","genre":"0","name":"505","rank_time":"0"},
-        {"id":"8","album":"Overexposed Track By Track","artist":"Maroon 5, Wiz Khalifa","duration":"3:51","genre":"0","name":"Payphone","rank_time":"1"},
-        {"id":"9","album":"A Head Full of Dreams","artist":"Coldplay","duration":"4:18","genre":"0","name":"Hymn for the Weekend","rank_time":"2"}
-    ]
-    
-    return render_template('explorer.html', songs=songs)
-
-@app.route('/explorer/', methods=['POST'])
-def search():
-    data = request.form
-    songs = [
-        {"id":"0","album":"Tribute","artist":"John Newman","duration":"4:00","genre":"0","name":"Love Me Again","rank_time":"2"},
-        {"id":"1","album":"V","artist":"Maroon 5","duration":"3:10","genre":"0","name":"Maps","rank_time":"1"},
-        {"id":"2","album":"A Head Full of Dreams","artist":"Coldplay","duration":"4:24","genre":"0","name":"Adventure of a Lifetime","rank_time":"1"},
-        {"id":"3","album":"Boulevard of Broken Dreams","artist":"Green Day","duration":"4:21","genre":"0","name":"Boulevard of Broken Dreams","rank_time":"2"},
-    ]
-    
-    return render_template('explorer.html', songs=songs)
+@app.route('/songs/')
+def songs():
+    return render_template('songs.html')
 
 @app.route('/store/')
 def store():
     return render_template('store.html')
+
+@app.route('/store/', methods=['GET'])
+def search():
+    data = request.form
+    content = data.get('query', type=str)
+    
+    return render_template('store.html', content=content)
 
 @app.route('/library/')
 def library():
@@ -57,6 +39,10 @@ def login():
 @app.route('/signup/')
 def signup():
     return render_template('signup.html')
+
+@app.route('/studio/song_upload')
+def song_upload():
+    return render_template('song_upload.html')
 
 
 if __name__ == '__main__':
